4e21b0808d6e8473e69bb2ac5b8bcad913442a5d
Camila Wior Gdud <wiorcamila@gmail.com>
2024-07-07 19:01:03 -0300

    ItemListContainer/Listo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import ItemCount from "../ItemCount/ItemCount.jsx"
-
+import "../ItemListContainer/ItemListContainer.css"
 
 const ItemListContainer = ( {titulo} ) =>{
     return (
         <>  
-            <div>{titulo}</div>
+            <div className="title-principal">{titulo}</div>
             <ItemCount stock={10} inicial={0}/>
         </>
     )

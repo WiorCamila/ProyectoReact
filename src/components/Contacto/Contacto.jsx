f1abfd2d2d0ca662fe201cac2e7e879a3d8f0be0
Camila Wior Gdud <wiorcamila@gmail.com>
2024-08-05 17:03:27 -0300

    Proyecto finalizado

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Row from 'react-bootstrap/Row';
 
 
@@ -28,64 +28,40 @@ const Contacto = () =>{
 
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Row className="mb-3">
-                    <Form.Group as={Col} md="4" controlId="validationCustom01">
-                        <Form.Label>First name</Form.Label>
+                    <Form.Group  as={Col} md="4" controlId="validationCustom01">
+                        <Form.Label>Nombre</Form.Label>
                         <Form.Control
                             required
                             type="text"
-                            placeholder="First name"
-                            defaultValue="Mark"
+                            placeholder="Meredith"
                         />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        <Form.Control.Feedback>Correcto.</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group as={Col} md="4" controlId="validationCustom02">
-                        <Form.Label>Last name</Form.Label>
+                        <Form.Label>Apellido</Form.Label>
                         <Form.Control
                             required
                             type="text"
-                            placeholder="Last name"
-                            defaultValue="Otto"
+                            placeholder="Grey"
                         />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                        <Form.Label>Username</Form.Label>
-                        <InputGroup hasValidation>
-                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                            <Form.Control
-                            type="text"
-                            placeholder="Username"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Please choose a username.
-                            </Form.Control.Feedback>
-                        </InputGroup>
+                        <Form.Control.Feedback>Correcto.</Form.Control.Feedback>
                     </Form.Group>
                 </Row>
                 <Row className="mb-3">
-                    <Form.Group as={Col} md="6" controlId="validationCustom03">
+                    <Form.Group as={Col} md="3" controlId="validationCustom03">
                         <Form.Label>City</Form.Label>
-                        <Form.Control type="text" placeholder="City" required />
+                        <Form.Control type="text" placeholder="MeredithGrey@gmail.com" required />
                         <Form.Control.Feedback type="invalid">
-                            Please provide a valid city.
+                            Por favor ingrese Email.
                         </Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group as={Col} md="3" controlId="validationCustom04">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control type="text" placeholder="State" required />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid state.
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="3" controlId="validationCustom05">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control type="text" placeholder="Zip" required />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid zip.
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                    <FloatingLabel
+                        controlId="floatingTextarea"
+                        label="Comments"
+                        className="mb-3"
+                    >
+                        <Form.Control as="textarea" placeholder="Leave a comment here" />
+                    </FloatingLabel>
                 </Row>
                 <Form.Group className="mb-3">
                     <Form.Check
